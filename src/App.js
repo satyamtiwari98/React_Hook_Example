296753f415c18fe0ea50b71a46ab800a19ad44d1
satyamtiwari98 <satyamtiwari1598@gmail.com>
2022-11-28 21:29:08 +0530

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const decrementBtnHandler = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
+  const incrementBtnHandler = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Satyam Tiwari</h1>
+      <div>
+        <button onClick={decrementBtnHandler} className='btn'>-</button>
+        <label>{count}</label>
+        <button onClick={incrementBtnHandler} className='btn'>+</button>
+      </div>
     </div>
   );
 }
